39b9c167da1e8fc3ab0120a6521e94a443d10b4c
redpandata19 <19ebe91@gmail.com>
2024-02-14 23:06:52 -0500

    question 8 complete

--- EmployeeSQL/SQl_Queries.sql
+++ EmployeeSQL/SQl_Queries.sql
@@ -68,3 +68,7 @@ WHERE employees.employee_id IN (SELECT employee_id
 
 
 -- 8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT COUNT(last_name) as Frequency, last_name
+FROM employees
+GROUP BY last_name
+ORDER BY Frequency desc;
