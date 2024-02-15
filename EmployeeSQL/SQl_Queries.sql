b7861f1d68ee4e6f9e12a9e38f6d8680242cfc30
redpandata19 <19ebe91@gmail.com>
2024-02-14 22:15:33 -0500

    question 2 complete

--- EmployeeSQL/SQl_Queries.sql
+++ EmployeeSQL/SQl_Queries.sql
@@ -4,9 +4,10 @@ FROM employees
 JOIN salaries ON
 employees.employee_id=salaries.employee_id;
 
-
-
 -- List the first name, last name, and hire date for the employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date >='1986-01-01'AND hire_date < '1987-01-01';
 
 -- List the manager of each department along with their department number, department name, employee number, last name, and first name.
 
