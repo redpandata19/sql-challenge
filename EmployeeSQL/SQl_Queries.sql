c05d93e7e3f682ae07aab6166365b5967f926c8f
redpandata19 <19ebe91@gmail.com>
2024-02-14 22:45:20 -0500

    question 4 complete

--- EmployeeSQL/SQl_Queries.sql
+++ EmployeeSQL/SQl_Queries.sql
@@ -23,16 +23,26 @@ ON department_managers.manager_employee_id = employees.employee_id
 JOIN departments
 ON department_managers.department_id = departments.department_id
 
--- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+-- 4. List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+SELECT 
+	department_employees.department_id, 
+	department_employees.employee_id, 
+	employees.last_name,
+	employees.first_name,
+	departments.department_name
+FROM department_employees
+JOIN employees
+ON department_employees.employee_id = employees.employee_id
+JOIN departments
+ON department_employees.department_id = departments.department_id
+
+-- 5. List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
 
 
--- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+-- 6. List each employee in the Sales department, including their employee number, last name, and first name.
 
 
--- List each employee in the Sales department, including their employee number, last name, and first name.
+-- 7. List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
 
--- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
-
--- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+-- 8. List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
