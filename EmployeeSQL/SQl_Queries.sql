b765d0c3eb1b1f90dfe1ee2bc9c22a9ff2df0b71
redpandata19 <19ebe91@gmail.com>
2024-02-14 22:27:26 -0500

    question 3 complete

--- EmployeeSQL/SQl_Queries.sql
+++ EmployeeSQL/SQl_Queries.sql
@@ -1,22 +1,38 @@
--- List the employee number, last name, first name, sex, and salary of each employee.
+-- 1. List the employee number, last name, first name, sex, and salary of each employee.
 SELECT employees.employee_id, employees.first_name, employees.sex, salaries.salary
 FROM employees
 JOIN salaries ON
 employees.employee_id=salaries.employee_id;
 
--- List the first name, last name, and hire date for the employees who were hired in 1986.
+-- 2. List the first name, last name, and hire date for the employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date >='1986-01-01'AND hire_date < '1987-01-01';
 
--- List the manager of each department along with their department number, department name, employee number, last name, and first name.
+-- 3. List the manager of each department along with their department number, department name, employee number, last name, and first name.
+SELECT 
+	department_managers.manager_employee_id, 
+	department_managers.department_id, 
+	departments.department_name, 
+	employees.employee_id, 
+	employees.last_name, 
+	employees.first_name
+FROM department_managers
+JOIN employees
+ON department_managers.manager_employee_id = employees.employee_id
+JOIN departments
+ON department_managers.department_id = departments.department_id
 
 -- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
 
+
 -- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+
 
 -- List each employee in the Sales department, including their employee number, last name, and first name.
 
+
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
 
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
